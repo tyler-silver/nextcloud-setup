cd4d179ab4fd688631e25aa564e6d08874eae4c6
Tyler <tyguy6203@gmail.com>
2025-01-05 16:21:29 -0600

    Create nextcloud-config.php

---
+++ config/nextcloud-config.php
@@ -0,0 +1,14 @@
+<?php
+$CONFIG = array (
+    'trusted_domains' => array (
+        0 => 'your-domain.com',
+    ),
+    'datadirectory' => '/var/www/nextcloud/data',
+    'dbtype' => 'mysql',
+    'dbname' => 'nextcloud',
+    'dbuser' => 'db_user',
+    'dbpassword' => 'db_password',
+    'dbhost' => 'localhost',
+    'dbtableprefix' => 'oc_',
+    'installed' => true,
+);
